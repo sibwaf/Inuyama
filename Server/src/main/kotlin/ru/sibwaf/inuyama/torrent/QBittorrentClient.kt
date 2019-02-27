01e995bd1331815938bffd1c7959c068c04894eb
sibwaf <7131069+sibwaf@users.noreply.github.com>
2019-02-27 16:46:37 +0300

    Fix spaces in directory path for QBittorrentClient

--- Server/src/main/kotlin/ru/sibwaf/inuyama/torrent/QBittorrentClient.kt
+++ Server/src/main/kotlin/ru/sibwaf/inuyama/torrent/QBittorrentClient.kt
@@ -3,17 +3,23 @@ package ru.sibwaf.inuyama.torrent
 import org.jsoup.Jsoup
 import org.kodein.di.Kodein
 import org.kodein.di.KodeinAware
+import java.net.URLEncoder
 
 class QBittorrentClient(override val kodein: Kodein) : TorrentClient, KodeinAware {
 
     // TODO: configuration
     override fun download(magnet: String, directory: String?) {
         Jsoup.connect("http://localhost:9091/command/download")
-                .data("urls", magnet)
                 .apply {
+                    val charset = request().postDataCharset()
+
+                    var body = "urls=${URLEncoder.encode(magnet, charset)}"
+
                     if (directory != null) {
-                        data("savepath", directory)
+                        body += "&savepath=${URLEncoder.encode(directory, charset).replace("+", "%20")}"
                     }
+
+                    requestBody(body)
                 }
                 .post()
     }
