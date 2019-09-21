85b4bae5ffd8c0cd26fb2d1bf58c9c88c309af51
sibwaf <7131069+sibwaf@users.noreply.github.com>
2019-09-21 22:43:21 +0300

    Display pairing info about server on startup

--- Server/src/main/kotlin/ru/sibwaf/inuyama/PairingManager.kt
+++ Server/src/main/kotlin/ru/sibwaf/inuyama/PairingManager.kt
@@ -3,13 +3,19 @@ package ru.sibwaf.inuyama
 import org.kodein.di.Kodein
 import org.kodein.di.KodeinAware
 import org.kodein.di.generic.instance
+import org.slf4j.LoggerFactory
 import ru.sibwaf.inuyama.common.DiscoverResponse
 import ru.sibwaf.inuyama.common.Pairing
+import ru.sibwaf.inuyama.common.utilities.humanReadable
 import java.net.DatagramPacket
 import java.net.DatagramSocket
 import kotlin.concurrent.thread
 
 class PairingManager(override val kodein: Kodein) : KodeinAware {
+
+    private companion object {
+        val logger = LoggerFactory.getLogger(PairingManager::class.java)!!
+    }
 
     private val keyKeeper by instance<KeyKeeper>()
     private val configuration by instance<InuyamaConfiguration>()
@@ -18,6 +24,8 @@ class PairingManager(override val kodein: Kodein) : KodeinAware {
         thread(isDaemon = true, name = "Discover request listener") {
             val socket = DatagramSocket(configuration.discoveryPort)
             val requestBuffer = Pairing.createDiscoverRequestBuffer()
+
+            logger.info("Listening for discover requests @ port ${configuration.discoveryPort} w/ key ${keyKeeper.keyPair.public.humanReadable}")
 
             while (true) {
                 val requestPacket = DatagramPacket(requestBuffer, requestBuffer.size)
