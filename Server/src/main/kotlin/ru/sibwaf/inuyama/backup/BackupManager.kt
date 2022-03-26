73ecc43b7404d196c8e3a50442671697bd629c2d
sibwaf <7131069+sibwaf@users.noreply.github.com>
2022-03-26 22:30:59 +0300

    server: fix possible backup overwrite

--- Server/src/main/kotlin/ru/sibwaf/inuyama/backup/BackupManager.kt
+++ Server/src/main/kotlin/ru/sibwaf/inuyama/backup/BackupManager.kt
@@ -6,6 +6,7 @@ import java.io.InputStream
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
+import java.nio.file.StandardOpenOption
 import java.time.Duration
 import java.time.LocalDateTime
 import java.time.OffsetDateTime
@@ -85,7 +86,7 @@ class BackupManager {
 
         log.debug("Writing a backup: $deviceId / $module")
 
-        Files.newOutputStream(path).use {
+        Files.newOutputStream(path, StandardOpenOption.CREATE_NEW).use {
             ZipOutputStream(it).use { zip ->
                 zip.putNextEntry(ZipEntry("data.bin"))
                 data.transferTo(zip)
