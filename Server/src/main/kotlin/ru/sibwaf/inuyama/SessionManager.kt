ac8d15f074ce67774a6dc6573e7f6f52f06b8056
sibwaf <7131069+sibwaf@users.noreply.github.com>
2022-05-22 18:53:05 +0300

    server: fix possible session overwriting

--- Server/src/main/kotlin/ru/sibwaf/inuyama/SessionManager.kt
+++ Server/src/main/kotlin/ru/sibwaf/inuyama/SessionManager.kt
@@ -2,27 +2,35 @@ package ru.sibwaf.inuyama
 
 import ru.sibwaf.inuyama.common.utilities.CommonUtilities
 import ru.sibwaf.inuyama.common.utilities.Cryptography
+import java.util.concurrent.ConcurrentHashMap
 import javax.crypto.SecretKey
 
 data class Session(val token: String, val key: SecretKey, val deviceId: String)
 
 // todo: [critical] session timeout
-// todo: [critical] session overwriting
 class SessionManager {
 
     private companion object {
         const val TOKEN_LENGTH = 64
     }
 
-    private val sessions = mutableMapOf<String, Session>()
+    private val sessions = ConcurrentHashMap<String, Session>()
 
     fun findSession(token: String): Session? = sessions[token]
 
     fun createSession(deviceId: String): Session {
-        val token = CommonUtilities.generateRandomString(TOKEN_LENGTH, CommonUtilities.ALPHABET_ALNUM)
+        val key = Cryptography.createAESKey()
 
-        return Session(token, Cryptography.createAESKey(), deviceId)
-            .also { sessions[token] = it }
+        var token: String
+        var session: Session
+        while (true) {
+            token = CommonUtilities.generateRandomString(TOKEN_LENGTH, CommonUtilities.ALPHABET_ALNUM)
+            session = Session(token, key, deviceId)
+            if (sessions.putIfAbsent(token, session) == null) {
+                break
+            }
+        }
+
+        return session
     }
-
 }
